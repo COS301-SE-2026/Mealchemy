8fab962967794db79194724135ed6c3419ff4ff5
Megan Azmanov <meganazmanov@gmail.com>
2026-05-02 12:25:56 +0200

    entry point to start sping boot

---
+++ backend/src/main/java/com/mealchemy/MealchemyApplication.java
@@ -0,0 +1,12 @@
+package com.mealchemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealchemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealchemyApplication.class, args);
+    }
+}
