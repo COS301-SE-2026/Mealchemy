727d5ba761e9fbf7e916278697d45834cc4e1841
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 17:52:30 +0200

    Basic VaultFolderRecipeController setup

---
+++ backend/src/main/java/com/mealchemy/vault/controller/VaultFolderRecipeController.java
@@ -0,0 +1,16 @@
+package com.mealchemy.vault.controller;
+
+/* Import libraries */
+import org.springframework.web.bind.annotation.*;
+
+/* Import classes */
+import com.mealchemy.vault.dto.VaultFolderRecipeRequest;
+import com.mealchemy.vault.dto.VaultFolderRecipeResponse;
+import com.mealchemy.vault.service.VaultFolderRecipeService;
+
+@RestController
+@RequestMapping("/Folder/Recipes")
+public class VaultFolderRecipeController
+{
+    
+}
