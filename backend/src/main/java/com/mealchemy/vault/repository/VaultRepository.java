eb08c06263d0ca2717d832bea1f5f94cd17c8e22
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 13:47:51 +0200

    Creating the repository for Vault

---
+++ backend/src/main/java/com/mealchemy/vault/repository/VaultRepository.java
@@ -0,0 +1,16 @@
+package com.mealchemy.vault.repository;
+
+/* Import libraries */
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.utils.Optional;
+import java.utils.List;
+
+/* Import classes */
+import com.mealchemy.vault.model.Vault;
+
+@Repository
+public interface VaultRepository extends JpaRepository<Vault, Long>
+{
+    List<VaultRepository> findByOwnerId(Long ownerId); 
+}
