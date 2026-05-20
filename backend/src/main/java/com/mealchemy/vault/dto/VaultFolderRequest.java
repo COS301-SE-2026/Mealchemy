f5e030519665e3641250858eb8e0011ad27bad6b
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 10:23:04 +0200

    Creating the DTO for vaultFolderRequests

--- backend/src/main/java/com/mealchemy/vault/dto/VaultFolderRequest.java
+++ backend/src/main/java/com/mealchemy/vault/dto/VaultFolderRequest.java
@@ -2,4 +2,32 @@ package com.mealchemy.vault.dto;
 
 public class VaultFolderRequest {
     
+    /* Variable declarations */
+
+    private Long vaultId;
+    private String folderName;
+
+    /* Getters */
+
+    public Long getVaultId()
+    {
+        return vaultId;
+    }
+
+    public String getFolderName()
+    {
+        return folderName;
+    }
+
+    /* Setters */
+
+    public void setVaultId(Long vaultIdIn)
+    {
+        vaultId = vaultIdIn;
+    }
+
+    public void setFolderName(String folderNameIn)
+    {
+        folderName = folderNameIn;
+    }
 }
