9ad0829b6c815cd948be6bb0c1e37255517fc2a3
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 13:31:00 +0200

    Creating JPA for vault to match what is done on the login and auth branch

---
+++ backend/src/main/java/com/mealchemy/vault/model/Vault.java
@@ -0,0 +1,84 @@
+package com.mealchemy.vault.model;
+
+/* Import libraries */
+
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+
+/* Import classes */
+
+import com.mealchemy.shared.enums.VaultType;
+
+@Entity
+@Table(name = "vaults")
+public class Vault
+{
+
+    /* Declaring fields */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vault_id")
+    private Long vaultId;
+    
+    @Column(name = "owner_id")
+    private Long ownerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "vault_type", nullable = false)
+    private VaultType vaultType = VaultType.PRIVATE;
+
+    @Column(nullable = false)
+    private String name = "My Vault";
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    /* Getters */
+
+    public Long getVaultId()
+    {
+        return vaultId;
+    }
+
+    public Long getOwnerId()
+    {
+        return ownerId;
+    }
+
+    public String getVaultType()
+    {
+        return vaultType;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public OffsetDateTime getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    /* Setters */
+
+    public void setOwnerId(Long ownerIdIn)
+    {
+        ownerId = ownerIdIn;
+    }
+
+    public void setVaultType(String vaultTypeIn)
+    {
+        if(Arrays.stream(VaultType.values()).map(Enum::name).anyMatch(vaultTypeIn::equals))
+        {
+            vaultType = vaultTypeIn;
+        }
+    }
+
+    public void setName(String nameIn)
+    {
+        name = nameIn;
+    }
+}
