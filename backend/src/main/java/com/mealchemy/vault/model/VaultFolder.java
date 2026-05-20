d1cc5445e3c8501e599642eccf16a3d3aa3ee2d0
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 09:22:15 +0200

    Creating the JPA model for vault_folders

--- backend/src/main/java/com/mealchemy/vault/model/VaultFolder.java
+++ backend/src/main/java/com/mealchemy/vault/model/VaultFolder.java
@@ -1,5 +1,65 @@
 package com.mealchemy.vault.model;
 
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = vault_folders)
 public class VaultFolder {
     
+    /* Declaring fields */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "folder_id")
+    private Long folderId;
+
+    @ManyToOne
+    @JoinColumn(name = "vault_id", nullable = false)
+    private Vault vault;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String folderName;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    /* Getters */
+
+    public Long getFolderId()
+    {
+        return folderId;
+    }
+    
+    public Vault getVault()
+    {
+        return vault;
+    }
+
+    public String getFolderName()
+    {
+        return folderName;
+    }
+
+    public OffsetDateTime getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    /* Setters */
+
+    public void setVault(Vault vaultIn)
+    {
+        vault = vaultIn;
+    }
+
+    public void setFolderName(String folderNameIn)
+    {
+        folderName = folderNameIn;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAtIn)
+    {
+        createdAt = createdAtIn;
+    }
 }
