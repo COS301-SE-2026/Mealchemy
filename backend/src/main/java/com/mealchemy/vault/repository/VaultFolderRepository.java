1f0c4227887846d952e756c7cb460b0aa591f796
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 10:37:48 +0200

    Set up basic repository for vaultFolder

--- backend/src/main/java/com/mealchemy/vault/repository/VaultFolderRepository.java
+++ backend/src/main/java/com/mealchemy/vault/repository/VaultFolderRepository.java
@@ -1,5 +1,16 @@
 package com.mealchemy.vault.repository;
 
-public class VaultFolderRepository {
-    
+/* Import libraries */
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
+
+/* Import classes */
+import com.mealchemy.vault.model.VaultFolder;
+
+@Repository
+public interface VaultFolderRepository extends JpaRepository<VaultFolder, Long> {
+    List<VaultFolder> findByVaultId(Long vaultId);
+    Optional<vaultFolder> findByFolderName(String folderName);
 }
