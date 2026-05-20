df53a00962d0c01c60998b04fe4e15df38c9d9f6
GabrielaBerimbau <giberimbau@gmail.com>
2026-05-20 21:42:09 +0200

    User preferences unit tests implemented

--- backend/src/main/java/com/mealchemy/auth/repository/UserProfileRepository.java
+++ backend/src/main/java/com/mealchemy/auth/repository/UserProfileRepository.java
@@ -3,7 +3,6 @@ package com.mealchemy.auth.repository;
 import com.mealchemy.auth.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-import java.util.List;
 
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer>{ //UserProfile object and Integer is profileId
