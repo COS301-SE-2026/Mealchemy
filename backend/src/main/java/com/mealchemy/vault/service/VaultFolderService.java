d6f557542948313ce97404876291865de489f187
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 11:00:06 +0200

    Basic vaultFolder service setup with mapping functions

---
+++ backend/src/main/java/com/mealchemy/vault/service/VaultFolderService.java
@@ -0,0 +1,46 @@
+package com.mealchemy.vault.service;
+
+/* Import libraries */
+import org.springframework.stereotype.Service;
+
+/* Import classes */
+import com.mealchemy.vault.model.VaultFolder;
+import com.mealchemy.vault.dto.VaultFolderRequest;
+import com.mealchemy.vault.dto.VaultFolderResponse;
+import com.mealchemy.vault.repository.VaultFolderRepository;
+
+@Service
+public class VaultFolderService {
+    private final VaultFolderRepository vaultFolderRepository;
+
+    public VaultFolderService(VaultFolderRepository vaultFolderRepository)
+    {
+        this.vaultFolderRepository = vaultFolderRepository;
+    }
+
+
+    /* Mapping functions */
+
+    private VaultFolderResponse mapToResponseDto(VaultFolder vaultFolderIn)
+    {
+        VaultFolderResponse response = new VaultFolderResponse();
+
+        response.setFolderId(vaultFolderIn.getFolderId())
+        response.setVaultId(vaultFolderIn.getVault().getVaultId());
+        response.setFolderName(vaultFolderIn.getFolderName());
+        response.setCreatedAt(vaultFolderIn.getCreatedAt());
+
+        return response;
+    }
+
+    private VaultFolder mapRequestToEntity(VaultFolderRequest request)
+    {
+        VaultFolder vaultFolder = new VaultFolder();
+
+        vaultFolder.setVaultId(request.getVaultId());
+        vaultFolder.setFolderName(request.getFolderName());
+
+        return vaultFolder;
+    }
+
+}
