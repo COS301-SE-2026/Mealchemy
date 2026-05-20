39b078f642a8cb826e06353f78bfd10291eddeec
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 17:59:29 +0200

    Mapping getRecipesByFolderId

--- backend/src/main/java/com/mealchemy/vault/controller/VaultFolderRecipeController.java
+++ backend/src/main/java/com/mealchemy/vault/controller/VaultFolderRecipeController.java
@@ -9,8 +9,22 @@ import com.mealchemy.vault.dto.VaultFolderRecipeResponse;
 import com.mealchemy.vault.service.VaultFolderRecipeService;
 
 @RestController
-@RequestMapping("/Folder/Recipes")
+@RequestMapping("/folder/recipes")
 public class VaultFolderRecipeController
 {
-    
+    private final VaultFolderRecipeService vaultFolderRecipeService;
+
+    public VaultFolderRecipeController(VaultFolderRecipeService vaultFolderRecipeService)
+    {
+        this.vaultFolderRecipeService = vaultFolderRecipeService;
+    }
+
+    /* Mapping Functions */
+
+    // Get
+    @GetMapping("/folderId")
+    public List<VaultFolderRecipeResponse> getRecipesByFolderId(@PathVariable Long folderId)
+    {
+        return vaultFolderRecipeService.getRecipesByFolderId(folderId);
+    }
 }
