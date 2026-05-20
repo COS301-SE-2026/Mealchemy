8247ec732b6030c3775dbe148238b1e391634551
PaulHofmeyr <u24618391@tuks.co.za>
2026-05-20 10:17:36 +0200

    Creating the DTO for vaultFolderResponses

--- backend/src/main/java/com/mealchemy/vault/dto/VaultFolderResponse.java
+++ backend/src/main/java/com/mealchemy/vault/dto/VaultFolderResponse.java
@@ -1,5 +1,58 @@
 package com.mealchemy.vault.dto;
 
+/* Import libraries */
+import java.time.OffsetDateTime;
+
 public class VaultFolderResponse {
     
+    /* Variable declarations */
+
+    private Long folderId;
+    private Long vaultId;
+    private String folderName;
+    private OffsetDateTime createdAt;
+
+    /* Getters */
+
+    public Long getFolderId()
+    {
+        return folderId;
+    }
+
+    public Long getVaultId()
+    {
+        return vaultId;
+    }
+
+    public String getFolderName()
+    {
+        return folderName;
+    }
+
+    public OffsetDateTime getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    /* Setters */
+
+    public void setFolderId(Long folderIdIn)
+    {
+        folderId = folderIdIn;
+    }
+
+    public void setVaultId(Long vaultIdIn)
+    {
+        vaultId = vaultIdIn;
+    }
+
+    public void setFolderName(String folderNameIn)
+    {
+        folderName = folderNameIn;
+    }
+
+    public void setCreatedAt(OffsetDatetime createdAtIn)
+    {
+        createdAt = createdAtIn;
+    }
 }
