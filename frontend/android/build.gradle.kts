7706e63be6b6abb024e1b98ec415e61eb812f38b
Megan235 <meganazmanov@gmail.com>
2026-05-21 23:25:31 +0200

    Update Android build configuration

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -19,6 +19,14 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Lock all dependency configurations to make versions predictable
+// This satisfies SonarCloud
+subprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
