54f2397cf34ef1c10a5007e7eb9e5b9c15279fc2
Megan235 <meganazmanov@gmail.com>
2026-05-21 23:10:17 +0200

    added device check so flutter http calls work on either macos or android depending

--- frontend/lib/core/providers/api_service_provider.dart
+++ frontend/lib/core/providers/api_service_provider.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../services/auth_interceptor.dart';
+
+//10.0.2.2 is Android emulator's alias for the host machine's localhost.
+//On macOS/iOS/desktop, use localhost directly, added a check to see machine
+String get _baseUrl =>
+    Platform.isAndroid ? 'http://10.0.2.2:8080' : 'http://localhost:8080';
 
 
 final authInterceptorProvider = Provider<AuthInterceptor>((ref) {
@@ -15,7 +22,7 @@ final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(
     BaseOptions(
 
-      baseUrl: 'http://10.0.2.2:8080',
+      baseUrl: _baseUrl,
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       headers: {
@@ -24,7 +31,7 @@ final dioProvider = Provider<Dio>((ref) {
       },
     ),
   );
-//TODO: Remove logging in production
+//Remove logging in production
   dio.interceptors.add(LogInterceptor(
     requestBody: true,
     responseBody: true,
