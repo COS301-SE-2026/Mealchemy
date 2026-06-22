a0c0cd2b33df9640b506b48308e15b52b9d931f6
u24957102 <u24957102@tuks.co.za>
2026-06-22 20:12:18 +0200

    feat(dashboard)  create the dashboard provider

---
+++ frontend/lib/features/dashboard/providers/dashboard_provider.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mealchemy/features/auth/models/user.dart';
+
+
+//State
+class DashboardState{
+  final bool isLoading;
+  final String? errorMessage;
+  final User? currentUser;
+
+  const DashboardState({
+    this.isLoading = false,
+    this.errorMessage,
+    this.currentUser,
+  });
+
+  DashboardState copyWith({
+    bool? isLoading,
+    String? errorMessage,
+    User? currentUser,
+  }) {
+    return DashboardState(
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: errorMessage,
+      currentUser: currentUser ?? this.currentUser,
+    );
+  }
+}
+
+//Notifier 
+class DashboardNotifier extends StateNotifier<DashboardState> {
+  DashboardNotifier() : super(const DashboardState());
+ 
+  Future<void> loadDashboard() async {
+    state =  state.copyWith(isLoading: true, errorMessage: null);
+ 
+    //Replace with real API call later 
+    await Future.delayed(const Duration(milliseconds: 600));
+ 
+    state = state.copyWith(
+      isLoading: false,
+      currentUser: const User(
+        userId: 1 ,
+
+        email: 'mutombo@mealchemy.com ',
+        displayName: 'Mutombo',
+
+        role: 'user',
+      ),
+    );
+  }
+}
