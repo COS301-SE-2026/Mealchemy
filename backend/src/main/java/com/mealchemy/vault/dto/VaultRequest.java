d7f6b1569d77f6d44985627b88a5519645704ef7
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-10 09:48:53 +0200

    Changing VaultRequest to a record

--- backend/src/main/java/com/mealchemy/vault/dto/VaultRequest.java
+++ backend/src/main/java/com/mealchemy/vault/dto/VaultRequest.java
@@ -3,50 +3,15 @@ package com.mealchemy.vault.dto;
 /* Import libraries */
 
 import java.time.OffsetDateTime;
-import java.util.Arrays;
 
 /* Import classes */
 
 import com.mealchemy.shared.enums.VaultType;
 
-public class VaultRequest
+public record VaultRequest(
+    @NotNull Integer ownerId,
+    @NotBlank VaultType vaultType,
+    @NotNull String name
+)
 {
-    /* Variable declarations */
-    private Integer ownerId;
-    private VaultType vaultType;
-    private String name;
-
-    /* Getters */
-
-    public Integer getOwnerId()
-    {
-        return ownerId;
-    }
-
-    public VaultType getVaultType()
-    {
-        return vaultType;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    /* Setters */
-
-    public void setOwnerId(Integer ownerIdIn)
-    {
-        ownerId = ownerIdIn;
-    }
-
-    public void setVaultType(VaultType vaultTypeIn)
-    {
-        vaultType = vaultTypeIn;
-    }
-
-    public void setName(String nameIn)
-    {
-        name = nameIn;
-    }
 }
