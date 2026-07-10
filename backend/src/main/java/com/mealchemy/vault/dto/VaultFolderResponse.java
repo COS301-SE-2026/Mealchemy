d350c88598d40ff0dfb63d04073c376012840c7b
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-10 10:36:50 +0200

    Changing VaultFolderResponse to record

--- backend/src/main/java/com/mealchemy/vault/dto/VaultFolderResponse.java
+++ backend/src/main/java/com/mealchemy/vault/dto/VaultFolderResponse.java
@@ -3,56 +3,20 @@ package com.mealchemy.vault.dto;
 /* Import libraries */
 import java.time.OffsetDateTime;
 
-public class VaultFolderResponse {
-    
-    /* Variable declarations */
-
-    private int folderId;
-    private int vaultId;
-    private String folderName;
-    private OffsetDateTime createdAt;
-
-    /* Getters */
-
-    public int getFolderId()
+public record VaultFolderResponse(
+    int folderId,
+    int vaultId,
+    String folderName,
+    OffsetDateTime createdAt
+)
+{
+    public static VaultFolderResponse from (VaultFolder vaultFolder) 
     {
-        return folderId;
-    }
-
-    public int getVaultId()
-    {
-        return vaultId;
-    }
-
-    public String getFolderName()
-    {
-        return folderName;
-    }
-
-    public OffsetDateTime getCreatedAt()
-    {
-        return createdAt;
-    }
-
-    /* Setters */
-
-    public void setFolderId(int folderIdIn)
-    {
-        folderId = folderIdIn;
-    }
-
-    public void setVaultId(int vaultIdIn)
-    {
-        vaultId = vaultIdIn;
-    }
-
-    public void setFolderName(String folderNameIn)
-    {
-        folderName = folderNameIn;
-    }
-
-    public void setCreatedAt(OffsetDateTime createdAtIn)
-    {
-        createdAt = createdAtIn;
+        return new VaultFolderResponse(
+            vaultFolder.getFolderId(),
+            vaultFolder.getVaultId(),
+            vaultFolder.getFolderId(),
+            vaultFolder.getCreatedAt()
+        );
     }
 }
