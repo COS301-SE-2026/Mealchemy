9cafb5622277cb4a55dda41e3daa8f8a6137b6b3
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-13 06:59:45 +0200

    Setting up model structure for recipeStep

---
+++ backend/src/main/java/com/mealchemy/recipe/model/RecipeStep.java
@@ -0,0 +1,18 @@
+package com.mealchemy.recipe.model;
+
+/* Import libraries */
+
+import jakarta.persistence.*;
+
+/* Import classes */
+
+@Entity
+@Table(name = recipe_steps)
+public class RecipeStep
+{
+    /* Declaring fields */
+
+    /* Getters */
+
+    /* Setters */
+}
