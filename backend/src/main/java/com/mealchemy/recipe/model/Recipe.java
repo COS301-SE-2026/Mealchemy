5c1773042c68ce3a990e096fee0ff61c78687eab
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-13 06:42:18 +0200

    Setting up model structure for recipe

---
+++ backend/src/main/java/com/mealchemy/recipe/model/Recipe.java
@@ -0,0 +1,19 @@
+package com.mealchemy.recipe.model;
+
+/* Import libraries */
+
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+
+/* Import Classes */
+
+@Entity
+@Table(name = "recipes")
+public class Recipe
+{
+    /* Declaring fields */
+
+    /* Getters */
+
+    /* Setters */
+}
