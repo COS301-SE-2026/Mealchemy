4482dfa2cac2dc30e04e9201f938c4868c45c1a7
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-14 13:43:56 +0200

    creating recipeIngredientService boilerplate

---
+++ backend/src/main/java/com/mealchemy/recipe/service/RecipeIngredientService.java
@@ -0,0 +1,17 @@
+import com.mealchemy.recipe.service;
+
+/* Import libraries */
+
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+/* Import classes */
+import com.mealchemy.recipe.model.RecipeIngredient;
+import com.mealchemy.recipe.dto.RecipeIngredientRequest;
+import com.mealchemy.recipe.dto.RecipeIngredientResponse;
+import com.mealchemy.recipe.repository.RecipeIngredientRepository;
+
+@Service
+public class RecipeIngredientService {
+    
+}
