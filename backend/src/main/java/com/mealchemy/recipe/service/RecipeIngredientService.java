01ebd5eb8e9208d8e0b2b266a43e36122622fd71
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-14 17:46:06 +0200

    Add mapRequestToEntity and createRecipeIngredient endpoint

--- backend/src/main/java/com/mealchemy/recipe/service/RecipeIngredientService.java
+++ backend/src/main/java/com/mealchemy/recipe/service/RecipeIngredientService.java
@@ -17,9 +17,37 @@ public class RecipeIngredientService
 {
     private final RecipeIngredientRepository recipeIngredientRepository;
 
-    public RecipeIngredienService(RecipeIngredientRepository recipeIngredientRepository)
+    public RecipeIngredientService(RecipeIngredientRepository recipeIngredientRepository)
     {
         this.recipeIngredientRepository = recipeIngredientRepository;
     }
 
+    // Create a new step for a specific recipe
+    public RecipeIngredientResponse createRecipeIngredient(RecipeIngredientRequest request, Recipe recipe)
+    {
+        // Add logic to check if recipe owner
+
+        RecipeIngredient recipeIngredientForReturn = mapRequestToEntity(request, recipe);
+        
+        return RecipeIngredientResponse.from(recipeIngredientRepository.save(recipeIngredientForReturn));
+    }
+
+    // Update a specific step in an existing recipe
+
+    // Delete a specific step in an existing recipe
+
+    /* Mapping functions */
+
+    public RecipeIngredient mapRequestToEntity(RecipeIngredientRequest request, recipe)
+    {
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+
+        recipeIngredient.setRecipe(recipe);
+        recipeIngredient.setIngId(request.ingId());
+        recipeIngredient.setQuantity(request.quantity());
+        recipeIngredient.setUnit(request.unit());
+        recipeIngredient.setSortOrder(request.sortOrder());
+
+        return recipeIngredient;
+    }
 }
