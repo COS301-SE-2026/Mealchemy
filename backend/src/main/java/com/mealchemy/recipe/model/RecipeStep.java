df98b805e3b586085021f923a349a9a73e67566e
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-14 06:35:17 +0200

    Declaring fields for recipeStep

--- backend/src/main/java/com/mealchemy/recipe/model/RecipeStep.java
+++ backend/src/main/java/com/mealchemy/recipe/model/RecipeStep.java
@@ -11,6 +11,19 @@ import jakarta.persistence.*;
 public class RecipeStep
 {
     /* Declaring fields */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "step_id")
+    private int stepId;
+
+    @Column(name = "recipe_id", nullable = false)
+    private int recipeId;
+
+    @Column(name = "step_nr", nullable = false)
+    private int stepNr;
+
+    @Column(name = "content", nullable = false)
+    private String content;
 
     /* Getters */
 
