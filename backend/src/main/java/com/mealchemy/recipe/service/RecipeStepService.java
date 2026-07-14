3f096d18ce0751af836eb31743f72f1018aac69a
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-14 13:48:11 +0200

    Creating recipeStepService boilerplate

---
+++ backend/src/main/java/com/mealchemy/recipe/service/RecipeStepService.java
@@ -0,0 +1,18 @@
+package com.mealchemy.recipe.service;
+
+/* Import libraries */
+
+import org.springframework.stereotype.Service;
+import java.util;
+
+/* Import classes */
+
+import com.mealchemy.recipe.model.RecipeStep;
+import com.mealchemy.recipe.dto.RecipeStepRequest;
+import com.mealchemy.recipe.dto.RecipeStepResponse;
+import com.mealchemy.recipe.repository.RecipeStepRepository;
+
+@Service
+public class RecipeStepService {
+    
+}
