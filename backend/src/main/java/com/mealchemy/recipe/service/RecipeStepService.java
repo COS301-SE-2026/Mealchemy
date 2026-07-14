8b69d3640a458016fc6de0ce5ce10aad773178ca
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-14 19:03:15 +0200

    Adding imports, preliminary comments, mapRequestToEntity function, and createRecipeStep endpoint

--- backend/src/main/java/com/mealchemy/recipe/service/RecipeStepService.java
+++ backend/src/main/java/com/mealchemy/recipe/service/RecipeStepService.java
@@ -5,15 +5,60 @@ package com.mealchemy.recipe.service;
 import org.springframework.stereotype.Service;
 import java.util;
 import java.util.stream.*;
+import org.springframework.web.server.*;
+import org.springframework.http.*;
 
 /* Import classes */
 
 import com.mealchemy.recipe.model.RecipeStep;
+import com.mealchemy.recipe.model.Recipe;
 import com.mealchemy.recipe.dto.RecipeStepRequest;
 import com.mealchemy.recipe.dto.RecipeStepResponse;
 import com.mealchemy.recipe.repository.RecipeStepRepository;
+import com.mealchemy.recipe.repository.RecipeRepository;
 
 @Service
 public class RecipeStepService {
-    
+
+    private final RecipeStepRepository recipeStepRepository;
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeStepService(RecipeStepRepository recipeStepRepository, RecipeRepository recipeRepository)
+    {
+        this.recipeStepRepository = recipeStepRepository;
+        this.recipeRepository = recipeRepository;
+    }
+
+    // Create a new step for a specific recipe
+    public RecipeStepResponse createRecipeStep(RecipeStepRequest request, Integer recipeId, Integer ownerId)
+    {
+        Recipe recipeToCheck = recipeRepository.findById(recipeId).orElseThrow(() -> new RuntimeException("Recipe not found."));
+
+        if(!recipeToCheck.getOwnerId().equals(ownerId))
+        {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Only the owner of this recipe can modify its steps.");
+        }
+
+        RecipeStep recipeStepForReturn = mapRequestToEntity(request, recipeToCheck);
+
+        return RecipeResponse.from(recipeStepRepository.save(RecipeStepForReturn));
+    }
+
+    // Update a specific step in an existing recipe
+
+    // Delete a specific step in an existing recipe
+
+    /* Mapping functions */
+
+    public RecipeStep mapRequestToEntity(RecipeStepRequest request, Recipe recipe)
+    {
+        RecipeStep recipeStep = new RecipeStep();
+
+        recipeStep.setRecipe(recipe);
+        recipeStep.setStepNr(request.stepNr());
+        recipeStep.setContent(request.content());
+
+        return recipeStep;
+    }
 }
