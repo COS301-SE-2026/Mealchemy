cd7680d9bce749a2979a6f46226807600c1ad35a
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-15 09:25:38 +0200

    Creating a model for the flavourProfileOption lookup table (cuisineTypes)

---
+++ backend/src/main/java/com/mealchemy/cuisinetype/model/FlavourProfileOptions.java
@@ -0,0 +1,56 @@
+package com.mealchemy.cuisinetype.model;
+
+/* Import libraries */
+
+import jakarta.persistence.*;
+import java.util.*;
+import org.hibernate.annotations.*;
+
+/* Import classes */
+
+@Entity
+@Table(name = "flavour_profile_options")
+public class FlavourProfileOptions()
+{
+    /* Declaring fields */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "value", nullable = false)
+    private String value;
+
+    @Column(name = "label", nullable = false)
+    private String label;
+
+    /* Getters */
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    public String getLabel()
+    {
+        return label;
+    }
+
+    /* Setters */
+
+    public void setValue(String valueIn)
+    {
+        value = valueIn;
+    }
+
+    public void setLabel(String labelIn)
+    {
+        label = labelIn;
+    }
+}
