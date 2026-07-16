700c50f5230689a32a87da1307447b5410eeeb68
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-16 14:34:13 +0200

    declaring fields in the vaultmember model

--- backend/src/main/java/com/mealchemy/vault/model/VaultMember.java
+++ backend/src/main/java/com/mealchemy/vault/model/VaultMember.java
@@ -5,15 +5,34 @@ package com.mealchemy.vault.model;
 import jakarta.persistence.*;
 import java.util.*;
 import java.time.OffsetDateTime;
-import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.*;
 import org.hibernate.type.SqlType;
 
 /* Import classes */
+
+import com.mealchemy.auth.model.User;
 
 @Entity
 @Table(name = "vault_members")
 public class VaultMember {
     /* Declaring fields */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "vault_id", nullable = false)
+    private Vault vault;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "joined_at", nullable = false)
+    private OffsetDateTime joinedAt;
 
     /* Getters */
 
