d14b03e3e9cb1c31bab7cedab22625dd3fbae181
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-16 15:41:21 +0200

    Creating VaultMemberRepository

---
+++ backend/src/main/java/com/mealchemy/vault/repository/VaultMemberRepository.java
@@ -0,0 +1,19 @@
+package com.mealchemy.vault.repository;
+
+/* Import libraries */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+/* Import classes */
+
+import com.mealchemy.vault.model.VaultMember;
+import com.mealchemy.auth.model.User;
+
+@Repository
+public interface VaultMemberRepository extends JpaRepository<VaultMember, Integer>
+{
+    List<VaultMember> findByVault_VaultId(Integer vaultId);
+    boolean existsByVault_VaultIdAndUser_UserId(Integer vaultId, Integer userId);
+}
