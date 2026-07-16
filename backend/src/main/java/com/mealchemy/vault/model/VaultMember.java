28337cd648a87eac42998798b755f79fb45dbb07
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-16 14:24:54 +0200

    Creating boilerplate for vaultMember model

---
+++ backend/src/main/java/com/mealchemy/vault/model/VaultMember.java
@@ -0,0 +1,21 @@
+package com.mealchemy.vault.model;
+
+/* Import libraries */
+
+import jakarta.persistence.*;
+import java.util.*;
+import java.time.OffsetDateTime;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlType;
+
+/* Import classes */
+
+@Entity
+@Table(name = "vault_members")
+public class VaultMember {
+    /* Declaring fields */
+
+    /* Getters */
+
+    /* Setters */
+}
