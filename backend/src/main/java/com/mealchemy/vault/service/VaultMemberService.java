b6da2d1dff0f86d8aed2a897a0a31ade728a201e
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-16 16:17:16 +0200

    Creating vaultMemberService and adding getAllVaultMembers

---
+++ backend/src/main/java/com/mealchemy/vault/service/VaultMemberService.java
@@ -0,0 +1,48 @@
+package com.mealchemy.vault.service;
+
+/* Import libraries */
+
+import org.springframework.stereotype.Service;
+import java.util.*;
+import java.util.stream.*;
+
+/* Import classes */
+
+import com.mealchemy.vault.model.VaultMember;
+import com.mealchemy.vault.dto.VaultMemberRequest;
+import com.mealchemy.vault.dto.VaultMemberResponse;
+import com.mealchemy.vault.repository.VaultMemberRepository;
+import com.mealchemy.auth.repository.UserRepository;
+
+@Service
+public class VaultMemberService {
+    private final VaultMemberRepository vaultMemberRepository;
+
+    private final UserRepository userRepository;
+
+    public VaultMemberService(VaultMemberRepository vaultMemberRepository, UserRepository userRepository)
+    {
+        this.vaultMemberRepisitory = vaultMemberRepository;
+        this.userRepository = userRepository;
+    }
+
+    // Get all vault members
+    public List<VaultMemberResponse> getAllVaultMembers()
+    {
+        return vaultMemberRepository.findAll().map(VaultMemberResponse::from).collect(Collectors.toList());
+    }
+
+    
+
+    /* Mapping functions */
+
+    public VaultMember mapRequestToEntity(VaultMemberRequest request)
+    {
+        VaultMember member = new VaultMember();
+
+        member.setVaultId();
+        member.setUserId(request.userId())
+
+        return member;
+    }
+}
