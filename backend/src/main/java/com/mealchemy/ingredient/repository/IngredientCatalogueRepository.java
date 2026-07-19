85f0aa135a4e91b51d30c4f1d8257a43ee017c5d
GabrielaBerimbau <giberimbau@gmail.com>
2026-07-19 22:35:31 +0200

    Ingredient Catalogue logic for frontend to get all ingredients in catalogue

--- backend/src/main/java/com/mealchemy/ingredient/repository/IngredientCatalogueRepository.java
+++ backend/src/main/java/com/mealchemy/ingredient/repository/IngredientCatalogueRepository.java
@@ -3,15 +3,35 @@
 package com.mealchemy.ingredient.repository;
 
 import com.mealchemy.ingredient.model.IngredientCatalogue;
+import com.mealchemy.ingredient.dto.IngredientCatalogueResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface IngredientCatalogueRepository extends JpaRepository<IngredientCatalogue, Integer> {
-    // when using ingredients catalogue - usually fininding ingredients by name
-    List<IngredientCatalogue> findByNameContaining(String name);
+    @Query("""
+            SELECT new com.mealchemy.ingredient.dto.IngredientCatalogueResponse(
+                c.ingId,
+                c.name,
+                cat.name
+            )
+            FROM IngredientCatalogue c
+            JOIN IngredientCategory cat ON c.categoryId = cat.categoryId
+            """)
+    List<IngredientCatalogueResponse> getIngredientCatalogueItems();
+
+    @Query("""
+        SELECT new com.mealchemy.ingredient.dto.IngredientCatalogueResponse(
+            c.ingId,
+            c.name,
+            cat.name
+        )
+        FROM IngredientCatalogue c
+        JOIN IngredientCategory cat ON c.categoryId = cat.categoryId
+        WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))
+    """)
+    List<IngredientCatalogueResponse> getIngredientByName(@Param("name") String name);
 }
