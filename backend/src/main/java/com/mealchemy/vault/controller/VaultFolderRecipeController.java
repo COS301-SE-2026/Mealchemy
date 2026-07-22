a67732e89f980261368de92cb5adab2008f750ac
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-22 14:51:51 +0200

    Updating the vaultFolderRecipecontroller's service method calls and parameters to reflect the updates to the methods in the vaultFolderRecipeService. Also updating the mapping where necessary

--- backend/src/main/java/com/mealchemy/vault/controller/VaultFolderRecipeController.java
+++ backend/src/main/java/com/mealchemy/vault/controller/VaultFolderRecipeController.java
@@ -2,7 +2,9 @@ package com.mealchemy.vault.controller;
 
 /* Import libraries */
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.util.*;
+import jakarta.validation.Valid;
 
 /* Import classes */
 import com.mealchemy.vault.dto.VaultFolderRecipeRequest;
@@ -24,43 +26,43 @@ public class VaultFolderRecipeController
 
     // Get
     @GetMapping("/recipes/{folderId}")
-    public List<VaultFolderRecipeResponse> getRecipesByFolderId(@PathVariable int folderId)
+    public List<VaultFolderRecipeResponse> getRecipesByFolderId(@PathVariable int folderId, @AuthenticationPrincipal String userId)
     {
-        return vaultFolderRecipeService.getRecipesByFolderId(folderId);
+        return vaultFolderRecipeService.getRecipesByFolderId(folderId, Integer.parseInt(userId));
     }
 
     // Get
     @GetMapping("/folders/{recipeId}")
-    public List<VaultFolderRecipeResponse> getFoldersByRecipeId(@PathVariable int recipeId)
+    public List<VaultFolderRecipeResponse> getFoldersByRecipeId(@PathVariable int recipeId, @AuthenticationPrincipal String userId)
     {
-        return vaultFolderRecipeService.getFoldersByRecipeId(recipeId);
+        return vaultFolderRecipeService.getFoldersByRecipeId(recipeId, Integer.parseInt(userId));
     }
 
     // Get
     @GetMapping("/{id}")
-    public VaultFolderRecipeResponse getFolderRecipeById(@PathVariable int id)
+    public VaultFolderRecipeResponse getFolderRecipeById(@PathVariable int id, @AuthenticationPrincipal String userId)
     {
-        return vaultFolderRecipeService.getFolderRecipeById(id);
+        return vaultFolderRecipeService.getFolderRecipeById(id, Integer.parseInt(userId));
     }
 
     // Post
-    @PostMapping
-    public VaultFolderRecipeResponse createVaultFolderRecipe(@RequestBody VaultFolderRecipeRequest request)
+    @PostMapping("/folder/{folderId}")
+    public VaultFolderRecipeResponse createVaultFolderRecipe(@Valid @RequestBody VaultFolderRecipeRequest request, @AuthenticationPrinciple String userId, @PathVariable Integer folderId)
     {
-        return vaultFolderRecipeService.createVaultFolderRecipe(request);
+        return vaultFolderRecipeService.createVaultFolderRecipe(request, Integer.parseInt(userId), folderId);
     }
 
     // Put
     @PutMapping("/{id}")
-    public VaultFolderRecipeResponse updateVaultFolderRecipe(@PathVariable int id, @RequestBody VaultFolderRecipeRequest request)
+    public VaultFolderRecipeResponse updateVaultFolderRecipe(@PathVariable int id, @RequestBody VaultFolderRecipeRequest request, @AuthenticationPrincipal String userId)
     {
-        return vaultFolderRecipeService.updateVaultFolderRecipe(id, request);
+        return vaultFolderRecipeService.updateVaultFolderRecipe(id, request, Integer.parseInt(userId));
     }
 
     // Delete
     @DeleteMapping("/{id}")
-    public void deleteVaultFolderRecipe(@PathVariable int id)
+    public void deleteVaultFolderRecipe(@PathVariable int id, @AuthenticationPrincipal String userId)
     {
-        vaultFolderRecipeService.deleteVaultFolderRecipe(id);
+        vaultFolderRecipeService.deleteVaultFolderRecipe(id, Integer.parseInt(userId));
     }
 }
