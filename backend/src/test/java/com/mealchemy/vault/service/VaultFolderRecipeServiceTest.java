dc65d8efee358a9de0627e396fa76144d8f555f8
PaulHofmeyr <u24618391@tuks.co.za>
2026-07-25 01:42:52 +0200

    Adding failure test for deleteVaultFolderRecipe when not member but added recipe

--- backend/src/test/java/com/mealchemy/vault/service/VaultFolderRecipeServiceTest.java
+++ backend/src/test/java/com/mealchemy/vault/service/VaultFolderRecipeServiceTest.java
@@ -409,6 +409,20 @@ public class VaultFolderRecipeServiceTest
     }
 
     @Test
+    void deleteVaultFolderRecipe_throwsException_whenNotMemberButAdded()
+    {
+        when(vaultFolderRecipeRepository.findById(1)).thenReturn(Optional.of(folderRecipe));
+        when(vaultMemberRepository.existsByVault_VaultIdAndUser_UserId(1, 1)).thenReturn(false);
+        
+        vault.setOwnerId(3);
+
+        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> vaultFolderRecipeService.deleteVaultFolderRecipe(1, 1));
+
+        assertEquals(HttpStatus.FORBIDDEN, ex.getStatusCode());
+        assertEquals("Only a vault member who added the recipe/vault owner can delete the folders.", ex.getReason());
+    }
+
+    @Test
     void deleteVaultFolderRecipe_throwsException_whenVaultFolderRecipeNotFound()
     {
         when(vaultFolderRecipeRepository.findById(99)).thenReturn(Optional.empty());
