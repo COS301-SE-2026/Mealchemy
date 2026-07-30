c6fb42cf013e8e61e05fa2b63960704730638201
sofiafinlayson <finlaysonsofia@gmail.com>
2026-07-30 14:57:44 +0200

    Pantry test fixes

--- frontend/lib/core/constants/app_config.dart
+++ frontend/lib/core/constants/app_config.dart
@@ -2,10 +2,10 @@ class AppConfig {
   static const bool useMockData = true;
 
 //if it's true the app will be on mock for the feature , false = real API for the feature
-  static const bool mockAuth = false; 
-  static const bool mockVault = false; 
+  static const bool mockAuth = false;
+  static const bool mockVault = false;
   static const bool mockRecipe = false;
-  static const bool mockPantry = true;
-  static const bool mockDiscovery = false; 
+  static const bool mockPantry = false;
+  static const bool mockDiscovery = false;
   static const bool mockShoppinglist = true;
 }
