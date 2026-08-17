6ae83783772e087a06125083337fa3f576358924
PaulHofmeyr <u24618391@tuks.co.za>
2026-08-17 22:48:53 +0200

    Adding repository functions to userCuisineAffinitiesRepository

--- backend/src/main/java/com/mealchemy/preference/repository/UserCuisineAffinitiesRepository.java
+++ backend/src/main/java/com/mealchemy/preference/repository/UserCuisineAffinitiesRepository.java
@@ -8,7 +8,8 @@ import com.mealchemy.preference.model.UserCuisineAffinities;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
-public interface UserCuisineAffinitiesRepository extends JpaRepository <UserCuisineAffinites, Integer>
+public interface UserCuisineAffinitiesRepository extends JpaRepository <UserCuisineAffinities, Integer>
 {
-    
+    List<UserCuisineAffinities> findAllByUserId(Integer userId);
+    Optional<UserCuisineAffinities> findByUserIdAndCuisineValue(Integer userId, String cuisineValue);
 }
