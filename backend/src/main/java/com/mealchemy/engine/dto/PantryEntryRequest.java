11d2e4cc62ac4402a356937314371b1978ad5993
PaulHofmeyr <u24618391@tuks.co.za>
2026-08-19 03:52:34 +0200

    Adding jsonPropertyAnnotations to pantryEntryRequest

--- backend/src/main/java/com/mealchemy/engine/dto/PantryEntryRequest.java
+++ backend/src/main/java/com/mealchemy/engine/dto/PantryEntryRequest.java
@@ -7,14 +7,15 @@ import jakarta.validation.constraints.*;
 import java.util.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record PantryEntryRequest(
-    @NotNull Integer ingId,
-    @NotNull Integer categoryId,
-    @NotNull BigDecimal quantity,
-    @NotBlank String unit,
-    @NotNull OffsetDateTime addedAt,
-    @NotNull Integer shelfLifeDays,
-    @NotBlank String storageLocation
+    @JsonProperty("ing_id") @NotNull Integer ingId,
+    @JsonProperty("category_id")  @NotNull Integer categoryId,
+    @JsonProperty("quantity") @NotNull BigDecimal quantity,
+    @JsonProperty("unit") @NotBlank String unit,
+    @JsonProperty("added_at") @NotNull OffsetDateTime addedAt,
+    @JsonProperty("shelf_life_days") @NotNull Integer shelfLifeDays,
+    @JsonProperty("storageLocation") @NotBlank String storageLocation
 )
 {}
