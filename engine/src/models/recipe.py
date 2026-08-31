8fe9113f187721d3bd30e537766e29f1745387b0
PaulHofmeyr <u24618391@tuks.co.za>
2026-08-31 07:00:34 +0200

    Adding validation to ingredient model, and adding nutrition model

--- engine/src/models/recipe.py
+++ engine/src/models/recipe.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class Ingredient(BaseModel):
-    ing_id: int
-    categoty_id: int
-    name: str
-    quantity: float
-    unit: str
+    ing_id: int = Field(gt = 0)
+    categoty_id: int = Field(gt = 0)
+    name: str = Field(min_length = 1)
+    quantity: float = Field(gt = 0)
+    unit: str = Field(min_length = 1)
+
+class Nutrition(BaseModel):
+    calories_kcal: Optional[int] = Field(default = None, ge = 0)
+    protein_g: Optional[float] = Field(default = None, ge = 0)
+    carbs_g: Optional[float] = Field(default = None, ge = 0)
+    fat_g: Optional[float] = Field(default = None, ge = 0)
