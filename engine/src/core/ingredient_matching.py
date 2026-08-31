73d3abf76307f15370c0faa4d610976022d76b60
PaulHofmeyr <u24618391@tuks.co.za>
2026-08-31 08:18:52 +0200

    Adding allergen check function

--- engine/src/core/ingredient_matching.py
+++ engine/src/core/ingredient_matching.py
@@ -1,5 +1,6 @@
 from src.models.recipe import Ingredient
 from src.models.user_state import PantryEntry
+from src.config import ALLERGEN_CATEGORY_MAP
 
 # pantry match function, will return set of owned ingredients as well as a set of not owned ingredients
 def pantry_ingredient_match(recipe_ingredients: list[Ingredient], pantry: list[PantryEntry]) -> tuple[set[int], set[int]]:
@@ -10,3 +11,18 @@ def pantry_ingredient_match(recipe_ingredients: list[Ingredient], pantry: list[P
     missing_ids = recipe_ids - pantry_ids
 
     return owned_ids, missing_ids
+
+# uses ingredients' category ids to check if it falls under an allergen group
+def allergen_check(recipe_ingredients: list[Ingredient], allergies: list[str]) -> bool:
+    for allergen_code in allergies:
+        blocked_category_ids = ALLERGEN_CATEGORY_MAP.get(allergen_code)
+
+        # If allergen code not mapped
+        if blocked_category_ids is None:
+            return False
+
+        for ingredient in recipe_ingredients:
+            if ingredient.category_id in blocked_category_ids:
+                return False
+
+    return True
