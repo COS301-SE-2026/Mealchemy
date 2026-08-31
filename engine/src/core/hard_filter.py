2d3d35eaa130e80c1f782cbff8089914b028d932
PaulHofmeyr <u24618391@tuks.co.za>
2026-08-31 08:50:09 +0200

    Created hard_filter.py

---
+++ engine/src/core/hard_filter.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timezone
+from src.config import DISLIKE_EXPIRY_DAYS
+from src.models.user_state import SwipeHistoryEntry, UserState
+from src.models.recipe import CandidatePoolEntry
+from src.core.ingredient_matching import allergen_check
+
+def passes_dietary_restrictions(dietary_tags: list[str], dietary_restrictions: list[str]) -> bool:
+    return all(restriction in dietary_tags for restriction in dietary_restrictions)
+
+def passes_dislike_time_check(recipe_id: int, swipe_history: list[SwipeHistoryEntry]) -> bool:
+    now = datetime.now(timezone.utc)
+
+    for swipe in swipe_history:
+        if swipe.recipe_id != recipe_id:
+            continue
+        if swipe.action != "DISLIKED":
+            continue
+
+    days_since = (now - swipe.swiped_at).days
+    if days_since < DISLIKE_EXPIRY_DAYS:
+        return False
+
+    return True
+
+def hard_filter(candidate_pool: list[CandidatePoolEntry], user_state: UserState) -> list[CandidatePoolEntry]:
+    return [
+        recipe for recipe in candidate_pool
+        if allergen_check(recipe.ingredients, user_state.allergies)
+        and passes_dietary_restrictions(recipe.dietary_tags, user_state.dietary_restrictions)
+        and passes_dislike_time_check(recipe.recipe_id, UserState.swipe_history)
+    ]
