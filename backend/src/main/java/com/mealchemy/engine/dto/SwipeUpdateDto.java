4c23190e9e03a813f6f0c6a20279317a65751fa1
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-01 21:50:20 +0200

    Creating SwipeUpdateDto

---
+++ backend/src/main/java/com/mealchemy/engine/dto/SwipeUpdateDto.java
@@ -0,0 +1,16 @@
+package com.mealchemy.engine.dto;
+
+/* Import classes */
+
+/* Import libraries */
+import jakarta.validation.constraints.*;
+import java.time.OffsetDateTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SwipeUpdateDto(
+    @JsonProperty("recipe_id") Integer recipeId,
+    String cuisine,
+    SwipeAction action,
+    @JsonProperty("signal_scores") SignalScoresResponse signalScores,
+    Double alpha
+){}
