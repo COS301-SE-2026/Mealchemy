ae478138c2a4c7e75c35c9dfb97a17fd749defdf
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-01 15:30:54 +0200

    Updating signalScoresResponse dto to reflect decisions made when creating the engine

--- backend/src/main/java/com/mealchemy/engine/dto/SignalScoresResponse.java
+++ backend/src/main/java/com/mealchemy/engine/dto/SignalScoresResponse.java
@@ -6,18 +6,18 @@ package com.mealchemy.engine.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record SignalScoresResponse(
-    @JsonProperty("pantry_coverage") Double pantryCoverage,
-    @JsonProperty("cuisine_affinity") Double cuisineAffinity,
+    @JsonProperty("pantry_match") Double pantryMatch,
+    @JsonProperty("cuisine") Double cuisine,
     Double nutrition,
     Double freshness,
     Double novelty
 )
 {
-    public static SignalScoresResponse from(Double pantryCoverageIn, Double cuisineAffinityIn, Double nutritionIn, Double freshnessIn, Double noveltyIn)
+    public static SignalScoresResponse from(Double pantryMatchIn, Double cuisineIn, Double nutritionIn, Double freshnessIn, Double noveltyIn)
     {
         return new SignalScoresResponse(
-            pantryCoverageIn,
-            cuisineAffinityIn,
+            pantryMatchIn,
+            cuisineIn,
             nutritionIn,
             freshnessIn,
             noveltyIn
