7c216ad7f286ca6e55dd6051511a29cd517ac994
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-01 19:30:04 +0200

    Creating swipe repository

---
+++ backend/src/main/java/com/mealchemy/swipes/repository/SwipeRepository.java
@@ -0,0 +1,14 @@
+package com.mealchemy.swipes.repository;
+
+/* Import classes */
+import com.mealchemy.swipes.model.Swipe;
+
+/* Import libraries */
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public interface SwipeRepository extends JpaRepository<Swipe, Integer> {
+    List<RecipeTags> findByUserId(Integer UserId);
+}
