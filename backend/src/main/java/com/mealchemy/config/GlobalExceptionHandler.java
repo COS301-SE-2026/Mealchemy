59e50dff0eb124725e2290121bc4e6fb555130b2
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-02 22:28:47 +0200

    Add map import

--- backend/src/main/java/com/mealchemy/config/GlobalExceptionHandler.java
+++ backend/src/main/java/com/mealchemy/config/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import com.mealchemy.engine.client.StaleStateException;
 import com.mealchemy.engine.client.InvalidSwipeException;
 
 import java.time.Instant;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
