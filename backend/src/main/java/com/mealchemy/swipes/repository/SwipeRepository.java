dbf2a73b654a641a918ea5a36c92ac2794b7cf1b
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-02 13:53:45 +0200

    Adding findbyUserIdAndAction to repository

--- backend/src/main/java/com/mealchemy/swipes/repository/SwipeRepository.java
+++ backend/src/main/java/com/mealchemy/swipes/repository/SwipeRepository.java
@@ -2,6 +2,7 @@ package com.mealchemy.swipes.repository;
 
 /* Import classes */
 import com.mealchemy.swipes.model.Swipe;
+import com.mealchemy.shared.enums.SwipeAction;
 
 /* Import libraries */
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface SwipeRepository extends JpaRepository<Swipe, Integer> {
     List<Swipe> findByUserId(Integer UserId);
     List<Swipe> findByUserIdAndFlushedFalse(Integer userId);
     long countByUserIdAndFlushedFalse(Integer userId);
+    List<Swipe> findByUserIdAndAction(Integer userId, SwipeAction action);
 }
