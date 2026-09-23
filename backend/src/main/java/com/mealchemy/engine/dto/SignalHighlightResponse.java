107a5e0f2705060c2b27f9d4b9ad0d6826b392e4
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-23 18:59:37 +0200

    Creating SignalHighlightResponse dto

---
+++ backend/src/main/java/com/mealchemy/engine/dto/SignalHighlightResponse.java
@@ -0,0 +1,12 @@
+package com.mealchemy.engine.dto;
+
+/* Import libraries */
+
+/* Import classes */
+
+public record SignalHighlightResponse(
+    String signal,
+    Integer percentage,
+    String message
+)
+{}
