97dddf3c27f6680c74ba69d5a0b6c4c89a30df8b
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-26 15:25:15 +0200

    Small semantic/syntax fixes

--- backend/src/main/java/com/mealchemy/mealprep/repository/MealPlanRepository.java
+++ backend/src/main/java/com/mealchemy/mealprep/repository/MealPlanRepository.java
@@ -2,14 +2,13 @@ package com.mealchemy.mealprep.repository;
 
 /* Import libraries */
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.*;
+import java.util.Optional;
 
 /* Import classes */
 import com.mealchemy.mealprep.model.MealPlan;
 
-public class MealPlanRepository extends JPARepository<MealPlan, Integer>{
+@Repository
+public interface MealPlanRepository extends JpaRepository<MealPlan, Integer>{
     Optional<MealPlan> findByVaultId(Integer vaultId);
 }
