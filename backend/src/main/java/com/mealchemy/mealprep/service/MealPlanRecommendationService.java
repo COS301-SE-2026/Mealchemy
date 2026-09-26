4a6c288517e31a209e3a0e73495e5922f297c381
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-26 19:52:48 +0200

    Creating MealPlanRecommendationService and setting up boilerplate

---
+++ backend/src/main/java/com/mealchemy/mealprep/service/MealPlanRecommendationService.java
@@ -0,0 +1,42 @@
+package com.mealchemy.mealprep.service;
+
+/* Import libraries */
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import java.time.LocalDate;
+import java.util.*;
+
+/* Import classes */
+import com.mealchemy.engine.dto.EnrichedRecommendationResponse;
+import com.mealchemy.engine.dto.PantryEntryRequest;
+import com.mealchemy.engine.service.RecommendationService;
+import com.mealchemy.mealprep.dto.DayRecommendationRequest;
+import com.mealchemy.mealprep.dto.DayRecommendationResponse;
+import com.mealchemy.mealprep.model.MealPlan;
+import com.mealchemy.mealprep.repository.MealPlanRepository;
+import com.mealchemy.preference.model.UserPreferences;
+import com.mealchemy.preference.repository.UserPreferencesRepository;
+import com.mealchemy.shared.enums.VaultType;
+import com.mealchemy.vault.model.Vault;
+import com.mealchemy.vault.repository.VaultRepository;
+
+@Service
+public class MealPlanRecommendationService {
+    private final RecommendationService recommendationService;
+    private final PantryProjectionService pantryProjectionService;
+    private final UserPreferencesRepository userPreferencesRepository;
+    private final MealPlanRepository mealPlanRepository;
+    private final VaultRepository vaultRepository;
+
+    public MealPlanRecommendationService(RecommendationService recommendationService,
+        PantryProjectionService pantryProjectionService, UserPreferencesRepository userPreferencesRepository,
+        MealPlanRepository mealPlanRepository, VaultRepository vaultRepository)
+    {
+        this.recommendationService = recommendationService;
+        this.pantryProjectionService = pantryProjectionService;
+        this.userPreferencesRepository = userPreferencesRepository;
+        this.mealPlanRepository = mealPlanRepository;
+        this.vaultRepository = vaultRepository;
+    }
+}
