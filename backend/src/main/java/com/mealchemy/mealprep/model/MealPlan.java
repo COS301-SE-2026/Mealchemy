6159ced67e1cb75d445b9a711ca9b1bd136335d3
PaulHofmeyr <u24618391@tuks.co.za>
2026-09-26 13:09:56 +0200

    Creating basic boilerplate for MealPlan model

---
+++ backend/src/main/java/com/mealchemy/mealprep/model/MealPlan.java
@@ -0,0 +1,42 @@
+package com.mealchemy.mealprep.model;
+
+/* Import libraries */
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+/* Import classes */
+
+public class MealPlan
+{
+    /* Declaring fields */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "plan_id")
+    private Integer planId;
+
+    @Column(name = "vault_id")
+    private Integer vaultIt;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+    @JoinColumn(mappedBy = "plan")
+    private List<MealPlanEntry> entries;
+
+    /* Getters */
+
+    /* Setters */
+}
